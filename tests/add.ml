64f6cce7a9bb64381a05761a8e9891385c366d71
Anton Novikov <an146@ya.ru>
2010-08-26 19:04:33 +0400

    simple test

---
+++ tests/add.ml
@@ -0,0 +1,12 @@
+open OUnit
+
+let add a b = a + b;;
+
+let text_fixture = "Add" >:::
+[
+	"add" >:: ( fun () ->
+		assert_equal 5 (add 2 3)
+	);
+]
+
+let _ = run_test_tt ~verbose:true text_fixture
