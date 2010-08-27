8f115974699be5d3aff57ab61647265eb1a3f42b
Anton Novikov <an146@ya.ru>
2010-08-27 19:00:06 +0400

    fix for arch linux extra/lablgtk2

---
+++ arch-linux-fix.sh
@@ -0,0 +1,18 @@
+# fixes ocamlfind unable to find lablgtk2
+[ `whoami` != "root" ] && {
+	echo please exec as root
+	exit 1
+}
+
+ln -s /usr/lib/ocaml/lablgtk2 /usr/lib/ocaml/site-lib
+
+cat > /usr/lib/ocaml/site-lib/lablgtk2/META <<END
+requires=""
+version="2.12.0"
+archive(byte)="lablgtk.cma"
+archive(byte,init)="lablgtk.cma gtkInit.cmo"
+archive(native)="lablgtk.cmxa"
+archive(native,init)="lablgtk.cmxa gtkInit.cmx"
+linkopts=""
+directory="+lablgtk2"
+END
