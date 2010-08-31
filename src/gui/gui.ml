2bd75023638a1ff5e0b07e0efed514c569894bce
Anton Novikov <an146@ya.ru>
2010-08-31 16:52:22 +0400

    menu bar

--- src/gui/gui.ml
+++ src/gui/gui.ml
@@ -1,3 +1,5 @@
+open GMain
+open GdkKeysyms
 
 let hello () =
    Midiio.output_note 0 60 1.0;
@@ -14,15 +16,30 @@ let delete_event ev =
     *)
    false;;
 
-let destroy () = GMain.Main.quit ();;
+let destroy () = Main.quit ();;
 
 let main () =
    let _ = GtkMain.Main.init () in
-   let window = GWindow.window ~border_width:10 () in
+
+   (* Window *)
+   let window = GWindow.window ~border_width:0 () in
    let _ = window#event#connect#delete ~callback:delete_event in
    let _ = window#connect#destroy ~callback:destroy in
-   let button = GButton.button ~label:"Hello World" ~packing:window#add () in
+   let vbox = GPack.vbox ~packing:window#add () in
+
+   (* Menu *)
+   let menubar = GMenu.menu_bar ~packing:vbox#pack () in
+   let factory = new GMenu.factory menubar in
+   let accel_group = factory#accel_group in
+
+   (* Menu -> File *)
+   let file_menu = factory#add_submenu "File" in
+   let factory = new GMenu.factory file_menu ~accel_group in
+   let _ = factory#add_item "Quit" ~key:_Q ~callback: Main.quit in
+
+   let button = GButton.button ~label:"Hello World" ~packing:vbox#add () in
    let _ = button#connect#clicked ~callback:hello in
+
    let print_device d = Printf.printf "Device: %s\n" d; flush stdout in
    Midiio.init ();
    let devices = Midiio.enum_output_devices () in
@@ -32,8 +49,9 @@ let main () =
    Midiio.set_program 0 0;
    Printf.printf "Set device: %s\n" device;
    flush stdout;
+   window#maximize ();
    window#show ();
-   GMain.Main.main ();
+   Main.main ();
    Midiio.fini ();;
 
 let _ = main ();;
