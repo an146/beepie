869f92b96dce21d210181a7bc5296a8c984d3066
Anton Novikov <an146@ya.ru>
2010-09-06 18:18:36 +0400

    tree balance test
    currently fails (no surprise, tree is a list)

--- src/tests/bstTest.ml
+++ src/tests/bstTest.ml
@@ -1,11 +1,26 @@
 open OUnit
 
-let test_add_ints () =
-   assert_equal (2 + 3) (Bst.add_ints 2 3);;
+module Bst = Bst.Make(struct
+	type t = int
+	let compare = compare
+end)
+
+let rec input_tree n =
+   if n <= 0 then
+      Bst.empty
+   else
+      Bst.add (Random.int 10000) (input_tree (n - 1))
+
+let input_tree =
+   Random.init 0x12345678;
+   input_tree 100000;;
+
+let test_balance () =
+   assert_equal 14 (Bst.height input_tree);;
 
 let tests =
 	"bst" >::: [
-      "add-ints" >:: test_add_ints
+      "balance" >:: test_balance
 	];;
 
 (* vim: set ts=3 sw=3 tw=80 : *)
--- src/util/bst.ml
+++ src/util/bst.ml
@@ -1,1 +1,15 @@
-let add_ints a b = a + b;;
+module type OrderedType =
+   sig
+      type t
+      val compare : t -> t -> int
+   end
+
+module Make(T : OrderedType) = struct
+   type elt = T.t
+   type t = elt list
+
+   let empty = []
+   let add element tree = element :: tree
+   let height tree = List.length tree
+end
+   
