42046e61fe0599c47a189745f52520db04895dda
Anton Novikov <an146@ya.ru>
2011-01-02 15:47:38 +0300

    menu.ml merged into mainWindow.ml

--- src/gui/mainWindow.ml
+++ src/gui/mainWindow.ml
@@ -1,3 +1,6 @@
+open GdkKeysyms
+open GMain
+
 let _ = GtkMain.Main.init ();;
 
 let window = GWindow.window ~border_width: 0 ();;
@@ -25,13 +28,104 @@ let add_file (f : MidiFile.file) =
    let _ = GMisc.label ~text ~packing:frame#add () in
    ignore f;;
 
-module Menu = Menu.Make(struct
-   let window = window
-   let add_file = add_file
-   let set_status = set_status
-end)
+let profile f =
+   let start_time = Unix.time () in
+   f ();
+   let time = Unix.time () -. start_time in
+   if time >= 0.1 then
+      set_status (Printf.sprintf "Done (%.3f s)" time)
+   else
+      set_status "Done";;
 
-let menu = new Menu.menu packing;;
+let new_file () =
+   add_file (MidiFile.File.create 240)
+
+let open_files () =
+   let filenames = FileDialog.get_open_filenames (window) in
+   let open_file fn = add_file (Import.import_file fn) in
+   profile (fun () -> List.iter open_file filenames)
+
+let do_test_sound () =
+   MidiIo.output_note 0 60 1.0;
+   MidiIo.output_note 0 60 1.0;
+   Printf.printf "bla\n";
+   flush stdout
+
+let test_sound () =
+   let _ = Thread.create do_test_sound () in
+   Printf.printf "xyi\n";
+   flush stdout
+
+class menu packing =
+   object (self)
+      val menubar = GMenu.menu_bar ~packing ()
+      val mutable m_output_device = None
+
+      method refresh_devices () =
+         let menu =
+            match m_output_device with
+              None -> failwith "menu not constructed"
+            | Some m -> m
+         in
+         List.iter (fun c -> c#destroy ()) menu#all_children;
+
+         let factory = new GMenu.factory menu in
+         let devices = MidiIo.enum_output_devices () in
+         let have_active = ref false in
+         let create_item device group =
+            let id = device.MidiIo.id in
+            let name = device.MidiIo.name in
+            let callback = fun set -> if set then MidiIo.set_output_device id in
+            let label = id ^ "    " ^ name in
+            let active = (MidiIo.get_output_device () = id) in
+            if active then have_active := true;
+            factory#add_radio_item label ~active ~callback ~group
+         in
+         let first = create_item (List.hd devices) None in
+         let create_item device = create_item device first#group in
+         let _ = List.map create_item (List.tl devices) in
+         if not !have_active then begin
+            first#set_active true;
+            MidiIo.set_output_device (List.hd devices).MidiIo.id
+         end
+
+      initializer
+         let factory = new GMenu.factory menubar in
+         let accel_group = factory#accel_group in
+
+         let wrap_errors f () =
+            set_status "";
+            try f () with
+                Failure desc -> set_status ("Error: " ^ desc)
+              | _ -> set_status "Unhandled exception occurred"
+         in
+
+         let add_item (factory : GMenu.menu GMenu.factory) callback =
+            factory#add_item ~callback: (wrap_errors callback)
+         in
+
+         (* File *)
+         let _ =
+            let m_file = factory#add_submenu "File" in
+            let factory = new GMenu.factory m_file ~accel_group in
+            let add_item = add_item factory in
+            let _ = add_item new_file   "New"     ~key:_N in
+            let _ = add_item open_files "Open..." ~key:_O in
+            let _ = add_item Main.quit  "Quit"    ~key:_Q in ()
+         in
+
+         (* Settings *)
+         let _ =
+            let m_settings = factory#add_submenu "Settings" in
+            let factory = new GMenu.factory m_settings ~accel_group in
+            let add_item = add_item factory in
+            m_output_device <- Some (factory#add_submenu "Output device");
+            let _ = add_item self#refresh_devices "Refresh devices" in
+            let _ = add_item test_sound "Test sound" in ()
+         in ()
+   end
+
+let menu = new menu packing;;
 let refresh_devices = menu#refresh_devices;;
 
 let init () =
