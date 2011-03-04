5851756cf6a0f86edc1356e5a7ddd20ca6cb5fe6
Anton Novikov <an146@ya.ru>
2011-03-05 00:29:06 +0300

    some layout

--- src/gui/tabWidget.ml
+++ src/gui/tabWidget.ml
@@ -7,16 +7,24 @@ open React
 module C = GnoCanvas
 module R = Gdk.Rectangle
 
-let strings = [40; 45; 50; 55; 59; 64]
-
 type tw = {
    file_s : file S.t;
    mutable tracks : track_id list;
-   mwidth : int DynArray.t;
+   mwidth : float DynArray.t;
    cnv : C.canvas;
    sw : GBin.scrolled_window;
    mutable rendered: (int, C.group) PMap.t;
 }
+
+let unitsize = 15.0
+
+let fl x = float_of_int x
+
+let track_height = 15.0
+
+let row_height tw = track_height *. (List.length tw.tracks |> fl)
+
+let strings = [40; 45; 50; 55; 59; 64]
 
 let file tw = S.value tw.file_s
 
@@ -49,103 +57,105 @@ let update_mwidth tw =
       let w =
          prerender tw m
          |> Enum.map snd |> Enum.flatten
-         |> Enum.map Enum.count |> Enum.sum
+         |> Enum.map Enum.count |> Enum.sum |> fl
       in
-      if w > 0 then w else 1
+      2. +. if w > 0. then w else 1.
    ) |> Enum.iter (DynArray.add tw.mwidth)
 
 let rows tw =
    if DynArray.empty tw.mwidth then
       update_mwidth tw;
-   let w = tw.cnv#width in
+   let w = fl tw.cnv#width /. unitsize in
    let i = ref 0 in
-   Printf.printf "measures: %i\n" (DynArray.length tw.mwidth);
    Enum.from (fun () ->
-      let s = ref 0 in
+      let s = ref 0. in
       let i_start = !i in
       (try
          while !s < w do
-            let min_part_width = 20 in
-            let ns = !s + (DynArray.get tw.mwidth !i) * min_part_width in
+            let ns = !s +. DynArray.get tw.mwidth !i in
             if ns > w then
                failwith "it's enough";
             s := ns;
             incr i
          done;
       with _ -> ());
-      if !s <= 0 then
+      if !s <= 0. then
          raise Enum.No_more_elements;
       assert (!i > i_start);
       (i_start -- (!i - 1))
    )
 
-let fl x = float_of_int x
-
 let expose tw r =
-   let track_height = 100 in
-   let row_height = track_height * List.length tw.tracks in
+   let rh = row_height tw in
    let r1, r2 =
-      let row y = y / row_height in
+      let row y = truncate (fl y /. unitsize /. rh) in
       row (R.y r), row (R.y r + R.height r)
    in
-   rows tw
+   Enum.combine (rows tw, (0 -- max_int))
    |> Enum.skip r1
    |> Enum.take (r2 - r1 + 1)
-   |> Enum.iteri (fun i e ->
-      let y = (i - 1) * row_height |> fl in
-      let parts =
-         Enum.clone e |> Enum.map (DynArray.get tw.mwidth) |> Enum.sum
+   |> Enum.iter (fun (e, i) ->
+      let y = fl i *. rh in
+      let rwidth =
+         Enum.clone e |> Enum.map (DynArray.get tw.mwidth) |> Enum.reduce (+.)
       in
-      let partw = (fl tw.cnv#width) /. (fl parts) in
-      let rx = ref 0 in
+      let stretch = (fl tw.cnv#width /. unitsize) /. rwidth in
+      let rx = ref 0. in
       let ms = F.measures (file tw) in
-      ignore partw;
       Enum.iter (fun m ->
-         Printf.printf "m: %i\n%!" m;
          let w = DynArray.get tw.mwidth m in
          let x = !rx in
-         let x' = x + w in
+         let x' = x +. w in
          if not (PMap.mem m tw.rendered) then (
-            let x = fl x and x' = fl x' and rh = fl row_height in
-            let g = C.group ~x:x ~y tw.cnv#root in
-            let _ = C.rect ~x1:x ~y1:y ~x2:x' ~y2:(y +. rh) ~fill_color:"red" g in
+            Printf.printf "m: %i\n%!" m;
+            let g = C.group ~x:(x *. stretch) ~y tw.cnv#root in
+            ignore x';
+            let _ =
+               C.rect ~x1:0. ~y1:0. ~x2:(w *. stretch) ~y2:rh ~fill_color:"red" g
+               ~props:[`OUTLINE_COLOR "black"]
+            in
             tw.rendered <- PMap.add m g tw.rendered
          );
          prerender tw (Vect.get ms m) |> ignore;
          rx := x'
       ) e
    );
-   Printf.printf "e: %i %i\n%!" (R.y r) (R.width r);
+   (*Printf.printf "e: %i %i\n%!" (R.y r) (R.width r);*)
    false
 
-let resize tw =
-   update_mwidth tw;
-   let sr = tw.cnv#get_scroll_region in
-   tw.cnv#set_scroll_region ~x1:sr.(0) ~y1:sr.(1) ~x2:sr.(2) ~y2:10000.0;
-   Printf.printf "w: %i\n%!" tw.cnv#width
+let resize tw {Gtk.width = rw} =
+   PMap.iter (fun _ g -> g#destroy ()) tw.rendered;
+   tw.rendered <- PMap.empty;
+   let w = fl rw /. unitsize in
+   let h = (rows tw |> Enum.count |> fl) *. row_height tw in
+   tw.cnv#set_scroll_region ~x1:0. ~y1:0. ~x2:w ~y2:h;
+   Printf.printf "w: %i\n%!" rw
 
 let vscroll tw a =
-   Printf.printf "y: %f\n%!" a#value
+   ()
+   (*Printf.printf "y: %f\n%!" a#value*)
 
 let create file_s =
    let tw =
       let sw = GBin.scrolled_window ~hpolicy:`NEVER () in
       let cnv = C.canvas ~packing:sw#add ~width:600 ~height:450 () in
-      cnv#set_center_scroll_region false;
+      (*cnv#set_center_scroll_region false;*)
+      cnv#set_pixels_per_unit unitsize;
       {
          file_s;
-         tracks = [1003] |> Obj.magic;
+         tracks = [1002] |> Obj.magic;
          mwidth = DynArray.create ();
          cnv;
          sw;
          rendered = PMap.empty;
       }
    in
-   GnoCanvas.rect tw.cnv#root
+   (*GnoCanvas.rect tw.cnv#root
       ~props:[ `X1 0.; `Y1 0.; `X2 600.; `Y2 450. ;
       `OUTLINE_COLOR "black" ; `WIDTH_UNITS 4. ] |> ignore;
+      *)
    expose_callback (fun _ ev -> expose tw (Expose.area ev)) tw.cnv;
-   resize_callback (fun _ _ -> resize tw) tw.cnv;
+   resize_callback (fun _ r -> resize tw r) tw.cnv;
    vadj_changed_callback (fun _ a -> vscroll tw a) tw.cnv;
    tw
 
