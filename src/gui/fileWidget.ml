804ed43e1b911a9fe51dc863dca3b7b53eb2681f
Anton Novikov <an146@ya.ru>
2011-05-29 19:16:31 +0400

    some code move

--- src/gui/fileWidget.ml
+++ src/gui/fileWidget.ml
@@ -26,6 +26,62 @@ let commit fw desc f =
    match hist fw with
    (l, r) -> fw.set_hist ((f, desc) :: l, [])
 
+let init_trtable_row fw i =
+   let attach j (exp, w) =
+      w#misc#set_can_focus false;
+      if j >= fw.trtable#columns then (
+         fw.trtable#set_columns (j + 1)
+      );
+      let expand =
+         match exp with
+         | `fill -> `NONE
+         | `expand -> `X
+      in
+      fw.trtable#attach ~left:j ~top:i ~expand w;
+   in
+   let btn_s lbl_s clb = button ~relief:`NONE ~callbacks:[
+      button_callback (fun _ _ -> clb (); false)
+   ] lbl_s in
+   let btn lbl = btn_s (S.const lbl) in
+   let sep () = `fill, separator `VERTICAL in
+
+   let track_s =
+      let eq (f, t) (f', t') = f == f' && t = t' in
+      S.map ~eq (fun f -> f, F.track i f) fw.file_s
+   in
+   let volume_s = S.map (F.volume) track_s in
+   let set_volume v =
+      let ft = S.value track_s in
+      if F.volume ft != v then
+         commit fw "Set Volume" (F.set_volume v ft)
+   in
+   let row = [
+      `fill,   btn (string_of_int (i + 1)) (fun () -> 
+         fw.tab#set_tracks [S.value track_s |> snd]
+      );
+      sep ();
+      `fill,   btn "M" (fun () -> ());
+      sep ();
+      `fill,   btn "S" (fun () -> ());
+      sep ();
+      `expand, btn_s (S.map (uncurry F.track_name) track_s) (fun () ->
+         Option.may (fun name ->
+            let f = track_s |> S.value |> uncurry (F.set_track_name name) in
+            commit fw "Set Track Name" f
+         ) (input_string ~title:"Set track name" "Enter track name:")
+      );
+      sep ();
+      `expand, btn "Instr" (fun () -> ());
+      sep ();
+      `expand, slider ~signal:(S.map float_of_int volume_s)
+      ~callback:(int_of_float |- set_volume)
+      ~move_callback:(Printf.printf "move: %f\n%!")
+      ~step_incr:1.0 ~page_incr:7.0
+      `HORIZONTAL (0.0, 127.0);
+   ] in
+   List.iteri attach row;
+   Stack.push (List.map snd row) fw.trtable_rows
+
 let set_tracks_count fw n =
    while Stack.length fw.trtable_rows > n do
       Stack.pop fw.trtable_rows |> List.iter (fun w -> w#destroy ())
@@ -33,59 +89,7 @@ let set_tracks_count fw n =
    let oldrows = Stack.length fw.trtable_rows in
    if n > 0 then fw.trtable#set_rows n;
    for i = oldrows to n - 1 do
-      let attach j (exp, w) =
-         w#misc#set_can_focus false;
-         let expand =
-            match exp with
-            | `fill -> `NONE
-            | `expand -> `X
-         in
-         if j >= fw.trtable#columns then
-            fw.trtable#set_columns (j + 1);
-         fw.trtable#attach ~left:j ~top:i ~expand w;
-      in
-      let btn_s lbl_s clb = button ~relief:`NONE ~callbacks:[
-         button_callback (fun _ _ -> clb (); false)
-      ] lbl_s in
-      let btn lbl = btn_s (S.const lbl) in
-      let sep () = `fill, separator `VERTICAL in
-
-      let track_s =
-         let eq (f, t) (f', t') = f == f' && t = t' in
-         S.map ~eq (fun f -> f, F.track i f) fw.file_s
-      in
-      let volume_s = S.map (F.volume) track_s in
-      let set_volume v =
-         let ft = S.value track_s in
-         if F.volume ft != v then
-           commit fw "Set Volume" (F.set_volume v ft)
-      in
-      let row = [
-         `fill,   btn (string_of_int (i + 1)) (fun () -> 
-            fw.tab#set_tracks [S.value track_s |> snd]
-         );
-         sep ();
-         `fill,   btn "M" (fun () -> ());
-         sep ();
-         `fill,   btn "S" (fun () -> ());
-         sep ();
-         `expand, btn_s (S.map (uncurry F.track_name) track_s) (fun () ->
-            Option.may (fun name ->
-               let f = track_s |> S.value |> uncurry (F.set_track_name name) in
-               commit fw "Set Track Name" f
-            ) (input_string ~title:"Set track name" "Enter track name:")
-         );
-         sep ();
-         `expand, btn "Instr" (fun () -> ());
-         sep ();
-         `expand, slider ~signal:(S.map float_of_int volume_s)
-                         ~callback:(int_of_float |- set_volume)
-                         ~move_callback:(Printf.printf "move: %f\n%!")
-                         ~step_incr:1.0 ~page_incr:7.0
-                         `HORIZONTAL (0.0, 127.0);
-      ] in
-      List.iteri attach row;
-      Stack.push (List.map snd row) fw.trtable_rows;
+      init_trtable_row fw i
    done
 
 let create initfile =
